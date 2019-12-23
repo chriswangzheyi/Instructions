7d2a0cf83b78b774fccf2d1331163987ec487ec1
chriswangzheyi <chriswangzheyi@hotmail.com>
2019-12-23 14:17:42 +0800

    springboot 集成druid

---
+++ Springboot/Springboot Druid/Demo/springboot_druid/src/test/java/com/wzy/springboot_druid/SpringbootDruidApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wzy.springboot_druid;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDruidApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
