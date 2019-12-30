3b3523544e60d457bc11766929238dd81e335f98
chriswangzheyi <chriswangzheyi@hotmail.com>
2019-12-30 18:29:55 +0800

    springcloud 最简demo

---
+++ SpringCloud/最简Demo/Code/springcloud_demo/eureka_demo2/src/main/java/com/wzy/eureka_demo2/EurekaDemo2Application.java
@@ -0,0 +1,15 @@
+package com.wzy.eureka_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemo2Application.class, args);
+    }
+
+}
